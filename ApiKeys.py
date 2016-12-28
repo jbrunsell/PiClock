3cc3c49df5aa50fb0fafcb40cdb06dc9eca14a82
Jason Brunsell <jason.brunsell@gmail.com>
2016-12-28 16:27:27 -0600

    Pulling files from old Raspberry Pi image

---
+++ ApiKeys.py
@@ -0,0 +1,5 @@
+#change this to your API keys
+# Weather Underground API key
+wuapi = 'd560b2c466156153'
+# Google Maps API key
+googleapi = ''   #Empty string, optional -- if you pull a small volume, you'll be ok
